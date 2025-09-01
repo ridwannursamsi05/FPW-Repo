78d37fc7f583a630b877e184e48cc5e9fb68b405
Ridwan Nursamsi <ridwannursamsi8@gmail.com>
2025-09-02 00:12:04 +0700

    Pertemuan 3 - Tugas Routing

---
+++ Laravel_10-Praktikum/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Tentang</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+
+    <a href="{{ route('contact.page') }}">Halaman Kontak</a><br>
+    <a href="/manage/edit-profile">Halaman Edit</a><br>
+    <a href="/pengguna/1">Halaman Pengguna</a><br>
+    <a href="/">Kembali</a>
+</body>
+</html>
