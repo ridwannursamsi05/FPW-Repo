ba6a6c4c349fb74118ae0b4ab00d9496825b121e
Ridwan Nursamsi <ridwannursamsi8@gmail.com>
2025-10-09 21:03:34 +0700

    Pertemuan 5 - Latihan Template Engine Blade

---
+++ Latihan/Pertemuan 5 - Template Engine Blade/Laravel_10-Praktikum/routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Basic Route
+Route::get('/about', function () {
+    return view('about');
+});
+
+// Named Route
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact.page');
+
+// ID Route
+Route::get('/pengguna/{id}', function ($id) {
+    $id= 1;
+    return "Profil Pengguna dengan ID: " . $id;
+});
+
+// Grouping route
+Route::prefix('manage')->group(function () {
+
+    Route::get('/edit-profile', function () {
+        return view ("manage.edit");
+    });
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Session Middleware Example
+Route::get('/rahasia', function() {
+    return "ini path rahasia";
+})->middleware(middleware: ['auth','RoleCheck:admin']);
+
+Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/route_count/{id}', [ProductController::class, 'show']);
+
+require __DIR__.'/auth.php';
