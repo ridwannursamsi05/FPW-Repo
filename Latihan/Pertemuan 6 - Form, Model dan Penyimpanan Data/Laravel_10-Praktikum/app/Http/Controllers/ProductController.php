66beec42c3e38d819a91af8c2c530e2ea0eea056
Ridwan Nursamsi <ridwannursamsi8@gmail.com>
2025-10-11 23:03:01 +0700

    Pertemuan 6 - Latihan Form, Model dan Penyimpanan Data

---
+++ Latihan/Pertemuan 6 - Form, Model dan Penyimpanan Data/Laravel_10-Praktikum/app/Http/Controllers/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $nama = "Mahasiswa Unsika";
+        // return view ('produk', ['nama'=> $nama, 'alertMessage' => 'Selamat belajar blade', 'alertType'=> 'success']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("master-data.product-master.create-product");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validasi input data
+        $validasi_data = $request->validate([
+            'product_name' => 'required|string|max:255',
+            'unit' => 'required|string|max:50',
+            'type' => 'required|string|max:50',
+            'information' => 'nullable|string',
+            'qty' => 'required|integer',
+            'producer' => 'required|string|max:255',
+        ]);
+
+        // Proses simpan data ke dalam database
+        Product::create($validasi_data);
+
+        return redirect()->back()->with('success', 'Product created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('barang', ['isi_data' => $id]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ Latihan/Pertemuan 6 - Form, Model dan Penyimpanan Data/Laravel_10-Praktikum/database/migrations/2025_10_07_042504_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // database/migrations/xxxx_xx_xx_create_products_table.php
+
+public function up(){
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('product_name');
+        $table->string('unit');
+        $table->string('type');
+        $table->text('information')->nullable();
+        $table->integer('qty');
+        $table->string('producer');
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
