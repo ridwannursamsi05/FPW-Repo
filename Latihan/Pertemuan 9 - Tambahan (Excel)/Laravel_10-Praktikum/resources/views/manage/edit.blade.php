6cf59119eec7a065345764ac1f4551b7903ec62b
Ridwan Nursamsi <ridwannursamsi8@gmail.com>
2025-11-03 18:55:56 +0700

    Pertemuan 9 - Latihan Tambahan (Excel)

---
+++ Latihan/Pertemuan 9 - Tambahan (Excel)/Laravel_10-Praktikum/resources/views/manage/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Edit</title>
+</head>
+<body>
+    <h1>Halaman Edit</h1>
+
+    <a href="/about">Kembali</a><br>
+</body>
+</html>
